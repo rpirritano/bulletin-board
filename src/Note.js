ae7429f6cc44fa247cf3f09898e1c71ae34f3f5f
Robert Pirritano <rpirritano@gmail.com>
2018-05-23 13:27:05 -0700

    added note.j

---
+++ src/Note.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import FaPencil from 'react-icons/lib/fa/pencil'
+import FaTrash from 'react-icons/lib/fa/trash'
+
+class Note extends Component {
+  constructor(props){
+    super(props)
+    this.edit = this.edit.bind(this)
+    this.remove = this.remove.bind(this)
+  }
+
+  edit(){
+    alert('editing note')
+  }
+  remove(){
+    alert('removing note')
+  }
+  render() {
+    return(
+      <div className="note">
+        <p>Build React Project</p>
+        <span>
+          <button onClick= {this.edit} id="edit"><FaPencil /></button>
+          <button onClick= {this.remove} id="remove"><FaTrash /></button>
+        </span>
+      </div>
+    )
+  }
+}
+
+export default Note
