fa823e9ab112dd1a2c68847fe9b1154e306302e0
Robert Pirritano <rpirritano@gmail.com>
2018-05-23 16:05:19 -0700

    added save and delete

--- src/Board.js
+++ src/Board.js
@@ -7,26 +7,46 @@ class Board extends Component {
 		this.state = {
 			notes: [
 				{
-					id: 33,
+					id: 0,
 					note: "Call Lisa"
 				},
 				{
-					id: 34,
+					id: 1,
 					note: "Email John"
 				},
 				{
-					id: 35,
+					id: 2,
 					note: "Order printer ink"
 				}
 			]
 		}
 		this.eachNote = this.eachNote.bind(this)
+    this.update = this.update.bind(this)
+    this.remove = this.remove.bind(this)
 	}
+
+  update(newText, i) {
+    console.log('updating item at index', i, newText)
+    this.setState(prevState => ({
+      notes: prevState.notes.map(
+        note => (note.id !== i) ? note : {...note, note:newText}
+      )
+    }))
+  }
+
+  remove(id) {
+    console.log('removing item at', id)
+    this.setState(prevState => ({
+      notes: prevState.notes.filter(note => note.id !== id)
+    }))
+  }
 
 	eachNote(note, i) {
 		return (
 			<Note key={i}
-				  index={i}>
+				  index={i}
+          onChange={this.update}
+          onRemove={this.remove}>
 				  {note.note}
 		    </Note>
 		)
--- src/Note.js
+++ src/Note.js
@@ -22,7 +22,7 @@ class Note extends Component {
 	}
 
 	remove() {
-		alert('removing note')
+		this.props.onRemove(this.props.index)
 	}
 
 	save(e) {
@@ -38,7 +38,7 @@ class Note extends Component {
 			<div className="note">
 				<form onSubmit={this.save}>
 					<textarea ref={input => this._newText = input}/>
-					<button><FaFloppyO /></button>
+					<button id="save"><FaFloppyO /></button>
 				</form>
 			</div>
 		)
