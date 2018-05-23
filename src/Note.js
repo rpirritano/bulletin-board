646aeac93485698b5ac503fbc2ad427d789222b9
Robert Pirritano <rpirritano@gmail.com>
2018-05-23 14:06:56 -0700

    added Board component

--- src/Note.js
+++ src/Note.js
@@ -25,16 +25,20 @@ class Note extends Component {
 		alert('removing note')
 	}
 
-	save() {
-		alert(this._newText.value)
+	save(e) {
+	 	e.preventDefault()
+	 	this.props.onChange(this._newText.value, this.props.index)
+	 	this.setState({
+			editing: false
+	 	})
 	}
 
 	renderForm() {
 		return (
 			<div className="note">
-				<form>
-					<textarea ref={input => this._newText = input} />
-					<button onClick={this.save}><FaFloppyO /></button>
+				<form onSubmit={this.save}>
+					<textarea ref={input => this._newText = input}/>
+					<button><FaFloppyO /></button>
 				</form>
 			</div>
 		)
@@ -43,7 +47,7 @@ class Note extends Component {
 	renderDisplay() {
 		return (
 			<div className="note">
-				<p>Learn React</p>
+				<p>{this.props.children}</p>
 				<span>
 					<button onClick={this.edit} id="edit"><FaPencil /></button>
 					<button onClick={this.remove} id="remove"><FaTrash /></button>
